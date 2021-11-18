0a9107f06aaf67bc4a9e95c963fdd5a4e1c15733
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-11-18 10:34:31 +0100

    Add Factorial Tests and Create Factorial Method

---
+++ solver.rb
@@ -0,0 +1,5 @@
+class Solver
+
+  def factorial
+  end
+end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,8 +1,20 @@
 require_relative '../solver'
 
 describe Solver do
+
   it "checks if this is an instance of the Solver class" do
     solve = Solver.new
     expect(solve).to be_instance_of Solver
   end
+
+  it "checks if the factorial method exists" do
+    solve = Solver.new
+    expect(solve).to respond_to :factorial
+  end
+
+  it "checks if factorial method behaves correctly" do
+  solve = Solver.new
+  five_factorial = solve.factorial(5)
+  expect(five_factorial).to be 120
+end
 end
